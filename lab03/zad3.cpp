a9759880265f6818d7cad72298593a9db01ef4dc
mwojciechowskii <mwojcio6@gmail.com>
2025-04-02 16:52:42 +0200

    updated zad3.cpp

--- lab03/zad3.cpp
+++ lab03/zad3.cpp
@@ -18,30 +18,24 @@ bool check_input_letters(char* dna){
 	return (*dna == 'A' || *dna == 'T' || *dna == 'C' || *dna == 'G');
 }
 
-//chwilke zajal mi pomysl rozwiazania rekurencyjnego
-bool substring_check(char* dna, char* substring){
+//wolalem rozwiazanie rekurencyjne :c
+int count_substring(char* dna, char* substring){
 
-	if (*substring =='\0')
-		return true;
-	if (*dna == '\0')
-		return false;
-	if (*dna == *substring)
-		return substring_check(dna + 1, substring + 1);
-	return false;
-}
-
-int count_substring(char* dna, char* substring, int* counts){
-
-	*counts = 0;
+	int counts = 0;
 	while (*dna){
 		if (!check_input_letters(dna) || !check_input_letters(substring)){
 			cout << "invalid input, try again" << endl;
 			return -1;
 		}
-		if (substring_check(dna, substring)) {
-			(*counts)++;
-		} dna++;
-	}return *counts;	
+		char* d = dna;
+        char* s = substring;
+        while (*s && *d && *s == *d) {
+                s++;
+                d++;
+            }if (*s == '\0') {
+                counts++;
+            }dna++;
+		}return counts; 
 }
 
 bool check_valid_input(char* dna){
@@ -56,17 +50,16 @@ int main(){
 	char dna[1024];
 	char substring[512];
 
-	int counts = 0;
-
 	while(1){
 		if (!check_valid_input(dna) || !check_valid_input(substring)){
 			break;
 		}	
-		if (count_substring(dna, substring, &counts) == -1){
+		int result = count_substring(dna, substring);
+		if (result == -1){
 			continue;
 		}else {
 		cout << "your dna: " << dna << "your substring: " << substring << endl;
-		cout << "appearances: " << counts << endl;
+		cout << "appearances: " << result << endl;
 		}
 	}
 	return 0;
